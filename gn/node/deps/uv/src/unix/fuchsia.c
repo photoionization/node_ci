7dde0d2f50e509e0384e89e7a363edf53519e121
Victor Gomes <victorgomes@chromium.org>
2020-04-17 13:08:01 +0200

    [uv] Adds build rules and a manifest for uv_test
    This CL also:
    - Removes unnecessary stub functions in fuchsia.c and include
      the correct files that implement them using a generic POSIX API
      in libuv.
    - Add features and services used by libuv in the node manifest.
    
    Bug: fuchsia:46889
    Change-Id: Id916ab936751ca452fd0318393f9274a36a2b93d
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/node-ci/+/2141672
    Commit-Queue: Victor Gomes <victorgomes@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>

--- gn/node/deps/uv/src/unix/fuchsia.c
+++ gn/node/deps/uv/src/unix/fuchsia.c
@@ -8,19 +8,6 @@ int uv_exepath(char* buffer, size_t* size) {
     return 0;
 }
 
-int uv_set_process_title(const char* title) {
-    return 0;
-}
-
-int uv_get_process_title(char* buffer, size_t size) {
-    *buffer = 0;
-    return 0;
-}
-
-char** uv_setup_args(int argc, char** argv) {
-    return argv;
-}
-
 void uv_loadavg(double avg[3]) {
 }
 
@@ -53,45 +40,5 @@ void uv_free_interface_addresses(uv_interface_address_t* addresses,
 }
 
 int uv_resident_set_memory(size_t* rss) {
-    return 0;
-}
-
-int uv_fs_event_init(uv_loop_t* loop, uv_fs_event_t* handle) {
-    return 0;
-}
-
-int uv_fs_event_start(uv_fs_event_t* handle,
-                      uv_fs_event_cb cb,
-                      const char* filename,
-                      unsigned int flags) {
-    return 0;
-}
-
-void uv__fs_event_close(uv_fs_event_t* handle) {
-}
-
-int uv__platform_loop_init(uv_loop_t* loop) {
-    return 0;
-}
-
-int uv__io_check_fd(uv_loop_t* loop, int fd) {
-    return 0;
-}
-
-int uv__io_fork(uv_loop_t* loop) {
-    assert(0 && "Fuchsia does not support fork()");
-    return 0;
-}
-
-void uv__io_poll(uv_loop_t* loop, int timeout) {
-}
-
-void uv__platform_invalidate_fd(uv_loop_t* loop, int fd) {
-}
-
-void uv__platform_loop_delete(uv_loop_t* loop) {
-}
-
-uint64_t uv__hrtime(uv_clocktype_t type) {
     return 0;
 }
