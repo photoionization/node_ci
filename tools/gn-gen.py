742268fc70d8ad27cd542750c69dfda4fadef9da
Jakob Linke <jgruber@chromium.org>
2023-04-25 10:30:02 +0200

    Temporarily disable node_use_node_code_cache
    See github.com/nodejs/node/issues/47636.
    
    If enabled, node creates the code cache on a transitory read-only heap
    state that may be invalidated when the heap snapshot is taken. This
    has worked so far, but will break in the near future. Disable this
    option until we have a solution in node.
    
    Note also the slight confusion between node_use_node_code_cache
    and node_use_code_cache: gn/node/BUILD.gn uses `node_use_code_cache`
    to set `NODE_USE_NODE_CODE_CACHE=1`.
    
    Bug: v8:13949
    Change-Id: Idbe1b18a36ff6eb6508dc6265fdb69418de04881
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/node-ci/+/4474327
    Commit-Queue: Patrick Thier <pthier@chromium.org>
    Reviewed-by: Patrick Thier <pthier@chromium.org>
    Auto-Submit: Jakob Linke <jgruber@chromium.org>

--- tools/gn-gen.py
+++ tools/gn-gen.py
@@ -52,7 +52,10 @@ def GenerateBuildFiles(options):
   gn_args.append('symbol_level=%s' % (1 if options.debug else 0))
   gn_args.append('use_goma=%s' % ToBool(options.goma))
   gn_args.append('is_component_build=%s' % ToBool(options.shared))
-  gn_args.append('node_use_code_cache=%s' % ToBool(not options.no_cache))
+  # TODO(jgruber): Reenable once code caching is fixed.
+  # See github.com/nodejs/node/issues/47636.
+  # gn_args.append('node_use_code_cache=%s' % ToBool(not options.no_cache))
+  gn_args.append('node_use_code_cache=false')
   gn_args.append('v8_enable_snapshot_compression=false')
   # Override system's OpenSSL configs for tests.
   gn_args.append('openssl_dir=""')
